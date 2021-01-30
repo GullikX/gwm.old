f7ae9a238814e6e76d3a395d5d267256e2a2bff2
Martin Gulliksson <martin@gullik.cc>
2021-01-30 20:35:46 +0000

    bump year

--- gwm.c
+++ gwm.c
@@ -1,4 +1,4 @@
-/* Copyright (C) 2019-2020 Martin Gulliksson <martin@gullik.cc>
+/* Copyright (C) 2019-2021 Martin Gulliksson <martin@gullik.cc>
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
